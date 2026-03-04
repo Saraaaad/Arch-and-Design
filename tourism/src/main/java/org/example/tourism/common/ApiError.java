b1ad94d4edb4106d0ca74d700c0143c63ddbdf16
SelinaAl-allam <202302917@bethlehem.edu>
2026-03-04 22:19:48 +0200

    add global exception handling and API error response structure

--- tourism/src/main/java/org/example/tourism/common/ApiError.java
+++ tourism/src/main/java/org/example/tourism/common/ApiError.java
@@ -1,4 +1,43 @@
 package org.example.tourism.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
 public class ApiError {
+    private HttpStatus status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+    private String message;
+    private String debugMessage;
+    private List<String> subErrors;
+
+    private ApiError() {
+        timestamp = LocalDateTime.now();
+    }
+
+    public ApiError(HttpStatus status) {
+        this();
+        this.status = status;
+    }
+
+    public ApiError(HttpStatus status, Throwable ex) {
+        this();
+        this.status = status;
+        this.message = "Unexpected error";
+        this.debugMessage = ex.getLocalizedMessage();
+    }
+
+    public ApiError(HttpStatus status, String message, Throwable ex) {
+        this();
+        this.status = status;
+        this.message = message;
+        this.debugMessage = ex.getLocalizedMessage();
+    }
 }
--- tourism/src/main/java/org/example/tourism/config/OpenApiConfig.java
+++ tourism/src/main/java/org/example/tourism/config/OpenApiConfig.java
@@ -1,4 +1,19 @@
 package org.example.tourism.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI employeeApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Booking Project API")
+                        .version("v1")
+                        .description("Booking project service API for pagination, sorting, and filtering."));
+    }
 }
